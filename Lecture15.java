6f5b5d9fa4c2c0cb3182bbf716e542f34dba1126
Mohammad Danish <100221645+danishwasil001@users.noreply.github.com>
2024-02-04 23:58:30 +0530

    Access Modifiers

--- Lecture15.java
+++ Lecture15.java
@@ -1,3 +1,6 @@
+//access modifiers, instances
+// all four types of access modifiers and it's uses
+
 public class A {
     int x=10;
     private int y=15;
@@ -13,10 +16,25 @@ public class A {
     }
 }
 
-
+/*
 public class B {
     public static void main(String[] args) {
         A ob1=new A();
     }
 }
+*/
 
+
+/*
+public class C extends A {
+    public static void main(String[] args) {
+        A ob2=new A();
+        //default and private are not accessible outside packge
+        //System.out.println(ob2.x);
+       // System.out.println(ob2.y);
+        // System.out.println(ob2.z);
+        // System.out.println(ob2.w);
+    }
+}
+
+*/
